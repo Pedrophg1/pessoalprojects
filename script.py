94c383718cad6aa2511928fd1b5a0761401d0df2
Pedrophg1 <130565668+Pedrophg1@users.noreply.github.com>
2023-05-23 19:10:32 -0300

    script para bot phython

---
+++ script.py
@@ -0,0 +1,17 @@
+import pyautogui as p
+import time
+p.PAUSE = 2
+p.press('win')
+p.write('desabilitarEthernet.bat')
+p.press('backspace')
+p.press('enter')
+time.sleep(5)
+p.press('win')
+p.write('habilitarEthernet.bat')
+p.press('backspace')
+p.press('enter')
+
+
+
+
+p.FAILSAFE = False
